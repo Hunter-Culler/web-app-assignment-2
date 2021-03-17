93cdb82850fff479771cc6d76df1a8350347bb41
ianmanderson92 <ianmanderson92@users.noreply.github.com>
2021-03-17 10:43:06 -0600

    added next/previous buttons

--- js/main.js
+++ js/main.js
@@ -6,23 +6,68 @@
 
 	March 17th, 2021
 
-	Status = Functional
+	Status = Functional, pageination work in progress
 */
 
+/*
 const app = Vue.createApp({
     data() {
         return {
             keyword: '',
+            currentIndex: 0,
+            result: null
+        }
+    },
+*/
+
+
+
+var app = Vue.createApp({
+    data() {
+        return {
+            keyword: '',
+            currentIndex: 0,
             result: null
         }
     },
     methods: {
         searchGoogleBooks() {
             //use this line for pagenation
-            fetch('https://www.googleapis.com/books/v1/volumes?q=' + this.keyword + "&startIndex=0&maxResults=20")
+            //fetch('https://www.googleapis.com/books/v1/volumes?q=' + this.keyword + "&startIndex=0&maxResults=20")
+
+            fetch('https://www.googleapis.com/books/v1/volumes?q=' + this.keyword + "&startIndex=" + this.currentIndex + "&maxResults=20")
                 .then(response => response.json())
                 .then(json => this.result = json)
 
+        },
+
+        nextPage() {
+                this.currentIndex += 20;
+
+                fetch('https://www.googleapis.com/books/v1/volumes?q=' + this.keyword + "&startIndex=" + this.currentIndex + "&maxResults=20")
+                    .then(response => response.json())
+                    .then(json => this.result = json)
+        },
+
+        prevPage() {
+            if (this.currentIndex < 20) {
+                this.currentIndex = 0;           
+            } else {
+                this.currentIndex -= 20;
+            }
+
+            fetch('https://www.googleapis.com/books/v1/volumes?q=' + this.keyword + "&startIndex=" + this.currentIndex + "&maxResults=20")
+                    .then(response => response.json())
+                    .then(json => this.result = json)
         }
     }
-})
+    //computed: {
+    //    getCurrentIndex() {
+    //        return this.currentIndex;
+    //    }
+    //}
+});
+
+//function nextPage() {
+//    app.$set(app.$data, 'currentIndex', app.getCurrentIndex + 20);
+//}
